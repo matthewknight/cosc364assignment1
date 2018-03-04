6241d6b6c99831c3f73dadf0ed068245b8dea4a0
Sam Boyles <sbo49@uclive.ac.nz>
2018-03-04 18:02:33 +1300

    Started to initialise sockets

--- rip_demon.py
+++ rip_demon.py
@@ -1,28 +1,47 @@
 import sys
 import json
-
+from socket import *
 class ripDemon(object):
+    """Creates an instance of a router that implements the RIP routing Daemon."""
     
     def __init__(self, filename):
         self.filename = filename
+        self.routing_id = None
+        self.input_ports = None
+        self.output_ports = None
         
-        
-
+    
     def load_config(self):
+        """Read the config file supplied via the command line."""
         data = json.load(open(self.filename))
-        routing_id = data["router-id"]
-        input_ports = data["input-ports"]
-        output_ports = data["outputs"]
-        return routing_id, input_ports, output_ports
+        self.routing_id = data["router-id"]
+        self.input_ports = data["input-ports"]
+        self.output_ports = data["outputs"]
+        return self.routing_id, self.input_ports, self.output_ports
+    
+    
+    def socket_creator(self):
+        """Initialise the sockets."""
+        
+        input_ports = list(self.input_ports)
+        print(input_ports)
+        pass
+    
+    
+    
+    def test_printr(self):
+        print("Routing id: " + self.routing_id)
+        print("Input ports: " + self.input_ports)
+        print("Output ports: " + self.output_ports)
+        
 
 
 
 if __name__ == "__main__":
     config_file_name = sys.argv[1]    
     router = ripDemon(config_file_name)
-    routing_id, input_ports, output_ports = router.load_config()
-    print("Routing id: " + routing_id)
-    print("Input ports: " + input_ports)
-    print("Output ports: " + output_ports)
+    router.load_config()
+    router.test_printr()
+    router.socket_creator()
     
         
