6c55c60083457fb6f9818038c891198066e0c518
Sam Boyles <sbo49@uclive.ac.nz>
2018-03-04 17:48:51 +1300

    Now loads command line parameters, changed to OO

--- rip_demon.py
+++ rip_demon.py
@@ -1,12 +1,28 @@
 import sys
+import json
 
-
-
-def load_config(argv):
+class ripDemon(object):
     
+    def __init__(self, filename):
+        self.filename = filename
+        
+        
+
+    def load_config(self):
+        data = json.load(open(self.filename))
+        routing_id = data["router-id"]
+        input_ports = data["input-ports"]
+        output_ports = data["outputs"]
+        return routing_id, input_ports, output_ports
 
 
 
-def main(argv):
-    #routing_id, input_ports, output_ports = load_config(argv)
+if __name__ == "__main__":
+    config_file_name = sys.argv[1]    
+    router = ripDemon(config_file_name)
+    routing_id, input_ports, output_ports = router.load_config()
+    print("Routing id: " + routing_id)
+    print("Input ports: " + input_ports)
+    print("Output ports: " + output_ports)
     
+        
