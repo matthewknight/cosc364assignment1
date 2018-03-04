dcb1fde3fcff01f3d152d300fbd13364c7567a95
Sam Boyles <sbo49@uclive.ac.nz>
2018-03-04 19:16:22 +1300

    Now creates UDP sockets for all input ports specified in config file

--- rip_demon.py
+++ rip_demon.py
@@ -2,14 +2,17 @@ import sys
 import json
 from socket import *
 class ripDemon(object):
-    """Creates an instance of a router that implements the RIP routing Daemon."""
+    """Creates an instance of a router that implements the RIP routing Daemon.
+    
+       Router can be initialised in the command line with:
+       
+           python rip_demon.py configX.json
+           
+       Where X is the config file to be used (1-10)
+    """
     
     def __init__(self, filename):
         self.filename = filename
-        self.routing_id = None
-        self.input_ports = None
-        self.output_ports = None
-        
     
     def load_config(self):
         """Read the config file supplied via the command line."""
@@ -20,18 +23,26 @@ class ripDemon(object):
         return self.routing_id, self.input_ports, self.output_ports
     
     
-    def socket_creator(self):
-        """Initialise the sockets."""
+    def input_socket_creator(self):
+        """Initialise the sockets and create a list of socket objects
+           based on the number of ports specified in config file"""
+        num_ports = len(self.input_ports)
+        self.sockets_list = []
         
-        input_ports = list(self.input_ports)
-        print(input_ports)
-        pass
+        for port in self.input_ports:
+            
+            serverSocket = socket(AF_INET, SOCK_DGRAM)
+            serverSocket.bind(('', port))
+            self.sockets_list.append(serverSocket)
+            print("waiting on port " + str(port))
+        
+        
     
     
     
     def test_printr(self):
         print("Routing id: " + self.routing_id)
-        print("Input ports: " + self.input_ports)
+        print(self.input_ports)
         print("Output ports: " + self.output_ports)
         
 
@@ -41,7 +52,7 @@ if __name__ == "__main__":
     config_file_name = sys.argv[1]    
     router = ripDemon(config_file_name)
     router.load_config()
-    router.test_printr()
-    router.socket_creator()
+    #router.test_printr()
+    router.input_socket_creator()
     
         
