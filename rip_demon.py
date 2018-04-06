3fb018b3942453b16cc30edd3e4ba61e29b9383c
Matthew <mkn29@uclive.ac.nz>
2018-04-06 14:55:45 +1200

    Created timer

--- rip_demon.py
+++ rip_demon.py
@@ -1,9 +1,13 @@
 import sys
 import json
 import select
+import threading
+import time
+import queue
 from socket import *
 
-class RipDemon(object):
+class RipDemon(threading.Thread):
+
     """Creates an instance of a router that implements the RIP routing Daemon.
     
        Router can be initialised in the command line with:
@@ -12,8 +16,9 @@ class RipDemon(object):
            
        Where X is the config file to be used (1-10)
     """
-    
+
     def __init__(self, filename):
+        threading.Thread.__init__(self)
         self.filename = filename
         data = json.load(open(self.filename))
         self.config_file_check(data)
@@ -33,13 +38,16 @@ class RipDemon(object):
 
     def listen(self):
         while True:
-            readable, writable, exceptional = select.select(self.input_sockets_list, [], [])
+            readable, writable, exceptional = select.select(self.input_sockets_list, [], [], 1.0)
             for s in readable:
                 print("Yeet")
                 # Got input from another demon
                     # 1. Unpackage data
                     # 2. Process data
                     # 3. Combine data into own data structure
+            if not sendScheduledMessageQueue.empty():
+                print("SENDING MESSAGE")
+                sendScheduledMessageQueue.queue.clear()
 
 
     def config_file_check(self, data):
@@ -66,12 +74,64 @@ class RipDemon(object):
         print(self.input_ports)
         print("Output ports: " + self.output_ports)
 
+    def run(self):
+        self.input_socket_creator()
+        self.listen()
+
+
+class RIPTimer(threading.Thread):
+    '''
+    create a thread object that will do the counting in the background
+    default interval is 1/1000 of a second
+    '''
+    def __init__(self, intervalBetweenMessages=1):
+        # init the thread
+        threading.Thread.__init__(self)
+        self.interval = intervalBetweenMessages  # seconds
+        # initial value
+        self.value = 0
+        # controls the while loop in method run
+        self.alive = False
+
+    def run(self):
+        '''
+        this will run in its own thread via self.start()
+        '''
+        self.alive = True
+        while self.alive:
+            time.sleep(1)
+            # update count value
+            self.value += 1
+            if (self.value == self.interval):
+                sendScheduledMessageQueue.put("YES")
+                self.value = 0
+            print(self.value)
+
+    def finish(self):
+        '''
+        close the thread, return final value
+        '''
+        # stop the while loop in method run
+        self.alive = False
+        return self.value
+
+
 
 if __name__ == "__main__":
-    config_file_name = sys.argv[1]    
+
+    sendScheduledMessageQueue = queue.Queue()
+
+    config_file_name = "config1.json"
     router = RipDemon(config_file_name)
+    timer = RIPTimer(5)
+
+    router.start()
+    timer.start()
+
+    router.join()
+    timer.join()
+
     #router.test_printr()
-    router.input_socket_creator()
-    router.listen()
+
 
 
